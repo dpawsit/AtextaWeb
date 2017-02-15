041913cd58459cd4cc36275d3c5e22418360f5dc
Rakan Nesheiwat <rnesh90@yahoo.com>
2017-02-14 18:59:49 -0800

    [prog] changed db source

--- database/config.js
+++ database/config.js
@@ -1,6 +1,16 @@
 const Sequelize = require('sequelize');
-const connection = require('../keys.js');
+const cred = require('../keys.js');
 
-var db = new Sequelize('mysql://admin:KLBMCAMIPBEDXHOV@aws-us-east-1-portal.9.dblayer.com:17114/compose');
+var db = new Sequelize('myAtexta', cred.username, cred.password, {
+  host: 'myatexta.c1qn5i5sh8u5.us-east-1.rds.amazonaws.com',
+  port : 3306,
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
 
 module.exports = db;
