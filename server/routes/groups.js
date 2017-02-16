6d9509ac55e1f72c680d6d51c0f62ffac4ec8fa1
Rakan Nesheiwat <rnesh90@yahoo.com>
2017-02-16 15:10:33 -0800

    [prog] almost completes server routers

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const gc = require('../../database/controllers/group_controllers');
 
-router.post('/newGroup', (req, res) => {
+router.post('/Group', (req, res) => {
   gc.CreateNewGroup(req.body.groupInfo, req.body.recipients)
   .then(result => {
     res.status(200).json(result);
@@ -12,7 +12,7 @@ router.post('/newGroup', (req, res) => {
   })
 })
 
-router.get('/userGroups/:userId', (req, res) => {
+router.get('/Groups/:userId', (req, res) => {
   gc.GetUserGroups(req.params.userId)
   .then(result => {
     res.status(200).json(result);
@@ -34,6 +34,26 @@ router.put('/groupName/:groupId/:groupName', (req, res) => {
 
 router.post('/recipientInfo', (req, res) => {
   gc.UpdateRecipientInfo(req.body.recipientId, req.body.recipientInfo)
+  .then(result => {
+    res.status(200).json(result);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+router.delete('/Recipient/:recipientId', (req, res) => {
+  gc.DeleteRecipient(req.params.recipientId)
+  .then(result => {
+    res.status(200).json(result);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+router.delete('/Group/:groupId', (req, res) => {
+  gc.DeleteGroup(req.parms.groupId)
   .then(result => {
     res.status(200).json(result);
   })
