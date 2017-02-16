eee8449961c593e81ac9e3494d15a1e33ea86905
Daniel Osaki <osaki.daniel@gmail.com>
2017-02-16 15:51:24 -0800

    [prog]start filling out forms for submitting messages/groups

---
+++ app/src/containers/AddMessageModal.jsx
@@ -0,0 +1,107 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Modal, ButtonToolbar, DropdownButton, MenuItem } from 'react-bootstrap'
+import { RaisedButton } from 'material-ui'
+
+class AddMessageModal extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			showModal: true,
+			step: 1,
+			selectedGroup: ''
+		}
+		this.stepDecider = this.stepDecider.bind(this)
+		this.incrementStep = this.incrementStep.bind(this)
+		this.closeModal = this.closeModal.bind(this)
+		this.clickGroup = this.clickGroup.bind(this)
+	}
+
+	incrementStep() {
+		this.setState({step: this.state.step+1})
+	}
+
+	closeModal() {
+		this.setState({showModal: false})
+	}
+
+	clickGroup(group) {
+		this.setState({selectedGroup: group})
+	}
+
+	stepDecider() {
+		const renderGroups = (group) => (
+				<li onClick = {()=>{this.clickGroup(group.group)}}className = "centered colorBox">{group.group}</li>
+			)
+		switch(this.state.step) {
+			case 1:
+				return (
+					<div>
+						<form>
+							<label>
+								What do you want to name this trigger?
+								<input type='text' id='groupName' />
+            	</label>
+            </form>
+						<RaisedButton type="button" label="Cancel" secondary={true} 
+						onClick = {this.closeModal}/>
+						<RaisedButton type="button" label="Next" secondary={true} 
+						onClick = {this.incrementStep}/>
+					</div>
+				)
+			case 2:
+				return (
+					<div>
+						<form>
+							<label>
+								Input the text you want to send
+								<input type='text' id='groupName' />
+            	</label>
+            </form>
+						<RaisedButton type="button" label="Cancel" secondary={true} 
+						onClick = {this.closeModal}/>
+						<RaisedButton type="button" label="Next" secondary={true} 
+						onClick = {this.incrementStep}/>
+					</div>
+				)
+			case 3:
+				return(
+					<div>
+						Select the group you want {this.state.selectedGroup}
+						<div className = "scrollable">
+							<ul>
+								{this.props.groups.map(renderGroups)}
+							</ul>
+						<RaisedButton type="button" label="Add new group" 
+						onClick = {this.incrementStep}/>
+						<RaisedButton type="button" label="Submit" 
+						onClick = {this.closeModal}/>
+						</div>
+					</div>
+				)
+			default:
+				return (
+					<div></div>
+				)
+		}
+	}
+
+	render() {
+		return(
+	    <Modal show={this.state.showModal} onHide={this.closeModal}>
+	    	<Modal.Header closeButton>
+	    		<Modal.Title>Add a message</Modal.Title>
+	    	</Modal.Header>
+	    	<Modal.Body>
+	    		{this.stepDecider()}
+	   		</Modal.Body>
+	    </Modal>
+		)
+	}
+}
+
+function mapStateToProps({ groups }) {
+	return { groups };
+}
+
+export default connect(mapStateToProps)(AddMessageModal)
--- app/src/containers/App.jsx
+++ app/src/containers/App.jsx
@@ -6,16 +6,20 @@ import Navbar from '../components/Navbar.jsx'
 import MessageList from './MessageList.jsx'
 import GroupList from './GroupList.jsx'
 import AddGroupModal from './addGroupModal.jsx'
+import AddMessageModal from './AddMessageModal.jsx'
  
 class App extends React.Component {
 	constructor(props) {
 		super(props)
 
 		this.state = {
-			showAddGroupModal: false
+			showAddGroupModal: false,
+			showAddMessageModal: false
 		}
 		this.closeAddGroupModal=this.closeAddGroupModal.bind(this)
 		this.toggleAddGroupModal=this.toggleAddGroupModal.bind(this)
+		this.toggleAddMessageModal = this.toggleAddMessageModal.bind(this)
+		this.closeAddMessageModal = this.closeAddMessageModal.bind(this)
 	}
 
 	closeAddGroupModal() {
@@ -26,6 +30,14 @@ class App extends React.Component {
 		this.setState({showAddGroupModal: !this.state.showAddGroupModal})
 	}
 
+	closeAddMessageModal() {
+		this.setState({showAddMessageModal: false})
+	}
+
+	toggleAddMessageModal() {
+		this.setState({showAddMessageModal: !this.state.showAddMessageModal})
+	}
+
 	render() {
 		return (
 			<div>
@@ -33,6 +45,8 @@ class App extends React.Component {
 					<div>
 						<Navbar	/>
 						<MessageList />
+						<FlatButton label="show the add message modal" onClick={this.toggleAddMessageModal} />
+						{this.state.showAddMessageModal ? <AddMessageModal /> : <div></div>}
 						<GroupList />
 						<FlatButton label="show the add group modal" onClick={this.toggleAddGroupModal} />
 						{this.state.showAddGroupModal ? <AddGroupModal /> : <div></div>}
