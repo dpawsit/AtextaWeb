91b5375403e80f8231b917b305ed3972480038f0
Rakan Nesheiwat <rnesh90@yahoo.com>
2017-02-19 00:50:35 -0800

    [feat] jwtToken in express

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const path = require('path')
+const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const database = require('../database/config.js')
 const helmet = require('helmet');
 const util = require('./server_util');
 const port = process.env.PORT || 3000;
+const jwt = require('jsonwebtoken');
+const config = require ('../keys').secret;
 
 const app = express()
-
+app.set('secret', config);
 app.use(helmet())
+app.use(morgan());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 // app.all('*', util.checkUser);
