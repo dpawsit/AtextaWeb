d22de5b9f3e78920f63a4789a181bbc3944ec01c
Daniel Osaki <osaki.daniel@gmail.com>
2017-03-02 19:25:03 -0800

    groupslist functionality may work now, if we are lucky

--- app/src/containers/AddGroupModal.js
+++ app/src/containers/AddGroupModal.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import axios from 'axios'
-import { addGroup } from '../actions/atexta_actions'
+import { addGroup, editGroup } from '../actions/atexta_actions'
 import { connect } from 'react-redux'
 import { Modal, ButtonToolbar, DropdownButton, MenuItem, Grid, Row, Col, Clearfix } from 'react-bootstrap'
-import { RaisedButton, FlatButton, Step, StepButton, StepContent, StepLabel, Stepper } from 'material-ui'
+import { RaisedButton, FlatButton, Step, StepButton, StepContent, StepLabel, Stepper, Table, TableBody, TableRow, TableRowColumn, TableHeader, TableFooter } from 'material-ui'
+
 
 class AddGroupModal extends React.Component {
 	constructor(props) {
@@ -11,11 +12,13 @@ class AddGroupModal extends React.Component {
 		this.state = {
 			step: 0,
 			recipientsToAdd: [],
-			newRecipientsToAdd: [],
+			// newRecipientsToAdd: [], this happens in another component now
 			newGroupMedium: 'Select a medium',
 			newGroupName: '',
 			fetchedValidRecipients: false,
-			validRecipients: []
+			validRecipients: [],
+			recipientsToAddQueue: [],
+			recipientsToRemoveQueue: []
 		}
 		this.componentWillMount = this.componentWillMount.bind(this)
 		this.stepDecider = this.stepDecider.bind(this)
@@ -28,6 +31,8 @@ class AddGroupModal extends React.Component {
 		this.fetchValidRecipients = this.fetchValidRecipients.bind(this)
 		this.handleGroupSubmit = this.handleGroupSubmit.bind(this)
 		this.decrementStep = this.decrementStep.bind(this)
+		this.addRecipientToQueue = this.addRecipientToQueue.bind(this)
+		this.removeRecipientFromQueue = this.removeRecipientFromQueue.bind(this) 	
 	}
 
 	componentWillMount() {
@@ -39,21 +44,32 @@ class AddGroupModal extends React.Component {
 			this.setState({
 				newGroupMedium: medium,	
 				newGroupName: this.props.initialData.name,
-				recipientsToAdd: this.props.initialData.recipients
+				recipientsToAdd: this.props.initialData.recipients.slice(),
+				step: 1
 			})
 		}
 	}
 
-
-
 	fetchValidRecipients() {
-		//refactor to a filter since recipients are on app state now
+		//REFACTOR to a filter since recipients are on app state now
 		let medium = this.state.newGroupMedium === 'Text' ? 'T' : 
 		this.state.newGroupMedium === 'Slack' ? 'S' : this.state.newGroupMedium === 'Email' ? 'E' : null
 		axios.get('groups/availableRecipients/' + this.props.userId + '/null/' + medium)
 		.then(result=>{
-			console.log('the result of fetchign valid recipients', result)
-			this.setState({validRecipients: result.data, fetchedValidRecipients: true})
+			let uniqueResults = []
+			//welp this time complexity sucks, change to objects later
+			for(let i = 0; i<result.data.length; i++) {
+				let flag = false;
+				for(let j = 0; j<this.state.recipientsToAdd.length; j++) {
+					if(this.state.recipientsToAdd[j].id === result.data[i].id) {
+						flag = true
+						break;
+					}
+				}
+				if(!flag) uniqueResults.push(result.data[i])
+			}
+			console.log(result.data)
+			this.setState({validRecipients: uniqueResults, fetchedValidRecipients: true})
 		})
 		.catch(err=>{
 			console.log('error fetching avilable recipients:', err)
@@ -74,20 +90,47 @@ class AddGroupModal extends React.Component {
 		}
 	}
 
+	addRecipientToQueue(indexes) {
+		indexes = indexes === 'none' ? [] : indexes
+ 		this.setState({recipientsToAddQueue: indexes})
+	}
 
-	addRecipientToGroup(recipient) {
-		let prevToAdd = this.state.recipientsToAdd
-		let prevAvailable = this.state.validRecipients
-		prevAvailable.splice(prevAvailable.indexOf(recipient), 1)
-		prevToAdd.push(recipient)
+	removeRecipientFromQueue(indexes) {
+		console.log('queried me', indexes)
+		indexes = indexes === 'none' ? [] : indexes
+		this.setState({recipientsToRemoveQueue: indexes})
+	}
+
+	addRecipientToGroup() {
+		let indexesOfValidRec = this.state.recipientsToAddQueue.slice()
+		console.log('trying to add', indexesOfValidRec)
+		let prevToAdd = this.state.recipientsToAdd.slice()
+		let prevAvailable = this.state.validRecipients.slice()
+		if(indexesOfValidRec==='all') {
+			this.setState({recipientsToAdd: prevToAdd.concat(prevAvailable), validRecipients: []})
+		} else {
+			//iterate backwards because indexes are ordered upwards, and push before splice
+			for(let i = indexesOfValidRec.length-1; i>=0; i--) {
+				prevToAdd.push(prevAvailable[indexesOfValidRec[i]])
+				prevAvailable.splice(indexesOfValidRec[i], 1)
+			}
+		}
 		this.setState({recipientsToAdd: prevToAdd, validRecipients: prevAvailable})
 	}
 
-	removeRecipientFromGroup(recipient) {
-		let prevToAdd = this.state.recipientsToAdd
-		let prevAvailable = this.state.validRecipients
-		prevToAdd.splice(prevToAdd.indexOf(recipient), 1)
-		prevAvailable.push(recipient)
+	removeRecipientFromGroup() {
+		let indexesOfRecToAdd = this.state.recipientsToRemoveQueue.slice()
+		console.log('trying to remove', indexesOfRecToAdd)
+		let prevToAdd = this.state.recipientsToAdd.slice()
+		let prevAvailable = this.state.validRecipients.slice()
+		if(indexesOfRecToAdd==='all') {
+			this.setState({recipientsToAdd: [], validRecipients: prevAvailable.concat(prevToAdd)})
+		} else {
+			for(let i = indexesOfRecToAdd.length-1; i>=0; i--) {
+				prevAvailable.push(prevToAdd[indexesOfRecToAdd[i]])
+				prevToAdd.splice(indexesOfRecToAdd[i], 1)
+			}
+		}
 		this.setState({recipientsToAdd: prevToAdd, validRecipients: prevAvailable})
 	}
 
@@ -111,76 +154,128 @@ class AddGroupModal extends React.Component {
 		this.state.newGroupMedium === 'Slack' ? 'S' : this.state.newGroupMedium === 'Email' ? 'E' : null
 		if(this.props.initialData) {
 			//this is an edit request
+			this.props.editGroup({
+				name: this.state.newGroupName,
+				mediumType: this.props.initialData.mediumType,
+				groupId: this.props.initialData.groupId,
+				recipients: this.state.recipientsToAdd
+			})
+			this.props.close()
+			console.log('recognized as an edit request')
+			const editedGroup = {
+				name: this.state.newGroupName,
+				mediumType: medium,
+				groupId: this.props.initialData.groupId,
+				savedRecipients: this.state.recipientsToAdd,
+				newRecipients: []
+			}
+
+			if(this.state.newGroupName !== this.props.initialData.name) {
+				axios.put('/groups/groupName', {
+					groupId: this.props.initialData.groupId,
+					groupName: this.state.newGroupName
+				})
+				.then(result=>{
+
+				})
+				.catch(err=>{
+
+				})
+			}
+
+			//do a deep check of initial recipients and recipients now
+			let toDelete = []
+			this.props.initialData.recipients.forEach(initialRecipient => {
+				let found = false;
+				this.state.recipientsToAdd.forEach(currentRecipient => {
+					if(currentRecipient.id === initialRecipient.id) found = true
+				})
+				if(!found) toDelete.push(initialRecipient.id)
+			})
+
+			let toAdd = []
+			this.state.recipientsToAdd.forEach(Irecipient => {
+				let found = false;
+				this.props.initialData.recipients.forEach(Jrecipient => {
+					if(Jrecipient.id === Irecipient.id) found = true
+				})
+				if(!found) toAdd.push(Irecipient.id)
+			}) 
+
+			if(toDelete.length) {
+				console.log('attempting to delete', toDelete)
+				axios.delete('/groups/groupRecipients', {params : {groupId : this.props.initialData.groupId, recipients: toDelete}
+				})
+				.then(result=>{
+					console.log('result of deleting recipients', result)
+				})
+				.catch(error=>{
+					console.log('error eleting group recipients', error)
+				})
+			}
+
+
+			if(toAdd.length) {
+				console.log('attempintg to add', toAdd)
+				axios.put('/groups/linkRecipient', {
+						groupId: this.props.initialData.groupId,
+						recipients: toAdd
+				})
+				.then(result=>{
+				})
+				.catch(error=>{
+
+				})
+			}
+
 		} else {
 			//this is a new post
-		}
-		axios.post('/groups/addGroup', {
-			groupInfo: {
-				name: this.state.newGroupName,
-				userId: this.props.userId,
-				mediumType: medium
-			},
-			newRecipients: this.state.newRecipientsToAdd,
-			savedRecipients: this.state.recipientsToAdd
-		})
-		.then(result=> {
-			console.log('group added', result)
-			this.props.addGroup({
-				name: result.data.group.name,
-				mediumType: result.data.group.mediumType,
-				groupId: result.data.group.id,
-				recipients: result.data.recipients.concat(this.state.recipientsToAdd)
+			axios.post('/groups/addGroup', {
+				groupInfo: {
+					name: this.state.newGroupName,
+					userId: this.props.userId,
+					mediumType: medium
+				},
+				newRecipients: [],
+				savedRecipients: this.state.recipientsToAdd
 			})
-			if(this.props.fromAddMessageModal) {
-				this.props.clickGroup(result.data.group.name, result.data.group.id)
-			}
-			this.props.close()
-		})
-		.catch(err=> {
-			console.log('error submitting group:', err)
-		})
+			.then(result=> {
+				console.log('group added', result)
+				this.props.addGroup({
+					name: result.data.group.name,
+					mediumType: result.data.group.mediumType,
+					groupId: result.data.group.id,
+					recipients: this.state.recipientsToAdd
+				})
+				if(this.props.fromAddMessageModal) {
+					this.props.clickGroup(result.data.group.name, result.data.group.id)
+				}
+				this.props.close()
+			})
+			.catch(err=> {
+				console.log('error submitting group:', err)
+			})
+		}
 	}
 
 	stepDecider() {
-		const renderAvailableRecipients = (recipient, i) => (
-			<div key={i}>
-				<Grid>
-					<Row>
-						<Col xs={5} md={5}>
-							<li onClick = {()=>{this.addRecipientToGroup(recipient)}} 
-							className = "centered colorBox">
-							{recipient.name}
-							</li>
-						</Col>
-						<Col xs={1} md={1}>
-							<RaisedButton type="button" label="Edit >" secondary={true} />
-						</Col>
-					</Row>
-				</Grid>
-			</div>
-		)
+		const renderAvailableRecipients = (recipient, i) => {
+			return(
+			<TableRow key={i} selected={false}>
+				<TableRowColumn>{recipient.name}</TableRowColumn>
+			</TableRow>
+			)}
 		const renderAddedRecipients = (recipient, i) => (
-			<div key={i}>
-				<Grid>
-					<Row>
-						<Col xs={5} md={5}>
-							<li onClick = {()=>{this.removeRecipientFromGroup(recipient)}} 
-							className = "centered colorBox">
-							{recipient.name}
-							</li>
-						</Col>
-						<Col xs={1} md={1}>
-							<RaisedButton type="button" label="Edit >" secondary={true} />
-						</Col>
-					</Row>
-				</Grid>
-			</div>
+			<TableRow key={i} selected={false}>
+				<TableRowColumn>{recipient.name}</TableRowColumn>
+			</TableRow>
 		)
 		switch(this.state.step) {
 			case 0:
 				return (
 					<div>
-						What medium is this?
+						<strong>What medium is this?</strong>
+						<p></p>
 						<ButtonToolbar>
 				      <DropdownButton title={this.state.newGroupMedium} id="dropdown-size-medium">
 				        <MenuItem eventKey="1" onSelect={()=> this.selectMediumType('Text')}>Text(Twilio)</MenuItem>
@@ -202,20 +297,52 @@ class AddGroupModal extends React.Component {
 					</div>
 				)
 			case 2:
+				let removeButton = <RaisedButton key="removeRec" label=">" onTouchTap={this.removeRecipientFromGroup} style={{padding : '2px'}}/>
+				let addButton = <RaisedButton key="addRec" label="<" onTouchTap={this.addRecipientToGroup} style={{padding : '2px'}}/>								
 				return this.state.fetchedValidRecipients ? (
 					<div>
-						who do you want to add to this group?
-						<div className = "scrollable">
-							Added so far:
-							<ul>
-								{this.state.recipientsToAdd.map(renderAddedRecipients)}
-							</ul>
-						</div>
-						<div className = "scrollable">
-							<ul>
-								{this.state.validRecipients.map(renderAvailableRecipients)}
-							</ul>
-						</div>
+						<Grid>
+							<Row>
+								<Col md={4}>
+									<Table height={400+'px'} selectable={true} multiSelectable={true}
+									onRowSelection = {this.removeRecipientFromQueue}>
+										<TableBody displayRowCheckbox={true} showRowHover={true}>
+											{this.state.recipientsToAdd.map(renderAddedRecipients)}
+										</TableBody>
+										<TableFooter>
+											<TableRow>
+												<TableRowColumn>
+													Added so far
+												</TableRowColumn>
+												<TableRowColumn>
+													{removeButton}
+												</TableRowColumn>
+											</TableRow>
+										</TableFooter>
+									</Table>
+									
+								</Col>
+								<Col md={4}>
+									<Table height={400+'px'} selectable={true} multiSelectable={true}
+									onRowSelection = {this.addRecipientToQueue}>
+										<TableBody displayRowCheckbox={true} showRowHover={true}>
+											{this.state.validRecipients.map(renderAvailableRecipients)}
+										</TableBody>
+										<TableFooter>
+											<TableRow>
+												<TableRowColumn>
+													{addButton}
+												</TableRowColumn>
+												<TableRowColumn>
+													All available
+												</TableRowColumn>
+											</TableRow>
+										</TableFooter>
+									</Table>
+									
+								</Col>
+							</Row>
+						</Grid>
 					</div>
 				) 
 				:
@@ -228,6 +355,8 @@ class AddGroupModal extends React.Component {
 	}
 
 	render() {
+		console.log('the list of crrent reci', this.state.recipientsToAdd)
+		console.log('other list of reci', this.state.validRecipients)
 		return(
 	    <Modal show={this.props.show} bsSize="large" onHide={this.props.close}>
 	    	<Modal.Header closeButton>
@@ -254,6 +383,7 @@ class AddGroupModal extends React.Component {
 	    		{this.stepDecider()}
 	   		</Modal.Body>
 				<Modal.Footer>
+					{/*REFACTOR = if it's edit mode, don't let them change the medium*/}
 					<FlatButton type="button" label={this.state.step === 0 ? "Cancel" : "Back" } 
 					onClick={this.state.step === 0 ? this.props.close : this.decrementStep}/>
 					<RaisedButton type="button" label={this.state.step=== 2 ? "Submit" : "Next"}
@@ -269,4 +399,4 @@ function mapStateToProps({ atexta }) {
 	return ({ userId: atexta.userId, userGroups: atexta.userGroups })
 }
 
-export default connect(mapStateToProps, {addGroup})(AddGroupModal)
+export default connect(mapStateToProps, {addGroup, editGroup})(AddGroupModal)
