aea444f9643859f0661edaf3208f3809dcb25ff7
Rakan Nesheiwat <rnesh90@yahoo.com>
2017-03-02 21:16:03 -0800

    admin controls complete

--- app/src/Admin/AdminControls.js
+++ app/src/Admin/AdminControls.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { adminLogout } from '../actions/admin_actions';
+import { browserHistory } from 'react-router';
+import { TextField, RaisedButton } from 'material-ui';
+import { MuiThemeProvider } from 'material-ui/styles';
+import { Alert } from 'react-bootstrap';
 
 const styles = {
     divider: {
@@ -23,25 +29,30 @@ class AdminControls extends Component {
       currentPassword : '',
       newPassword : '',
       confirmPassword : '',
+      showPassAlert: false,
       showAlert : false,
       alertMessage : '',
       adminUsername: '',
       adminPassword: '',
-      adminConfirmPassword: ''
+      adminConfirmPassword: '',
+      successAlert: false
     }
-    this.handleAdminConfirmPassword = this.handleAdminConfirmPassword.bind(this);
+    this.handleCurrentPassword = this.handleCurrentPassword.bind(this);
     this.handleNewPassword = this.handleNewPassword.bind(this);
-    this.handleAdminConfirmPassword = this.handleAdminConfirmPassword.bind(this);
+    this.handleConfirmPassword = this.handleConfirmPassword.bind(this);
     this.handleAlert = this.handleAlert.bind(this);
     this.handleAdminPassword = this.handleAdminPassword.bind(this);
     this.handleAdminConfirmPassword = this.handleAdminConfirmPassword.bind(this);
     this.handleAdminUsername = this.handleAdminUsername.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleNewAdmin = this.handleNewAdmin.bind(this);
+    this.handlePassAlert = this.handlePassAlert.bind(this);
   }
 
   handleCurrentPassword(e){
     this.setState({
       currentPassword : e.target.value,
-      showAlert : false,
+      showPassAlert : false,
       alertMessage : ''
     })
   }
@@ -49,27 +60,34 @@ class AdminControls extends Component {
   handleNewPassword(e){
     this.setState({
       newPassword : e.target.value,
-      showAlert : false,
+      showPassAlert : false,
       alertMessage : ''
     })
   }
 
-    handleConfirmPassword(e){
+  handleConfirmPassword(e){
     this.setState({
       confirmPassword : e.target.value,
-      showAlert : false,
+      showPassAlert : false,
       alertMessage : ''
     })
   }
 
-    handleAlert(message){
+  handleAlert(message){
     this.setState({
       showAlert : true,
       alertMessage : message
     })
   }
 
-    handleAdminPassword(e){
+  handlePassAlert(message){
+    this.setState({
+      showPassAlert : true,
+      alertMessage : message
+    })
+  }
+
+  handleAdminPassword(e){
     this.setState({
       adminPassword : e.target.value,
       showAlert : false,
@@ -77,7 +95,7 @@ class AdminControls extends Component {
     })
   }
 
-    handleAdminConfirmPassword(e){
+  handleAdminConfirmPassword(e){
     this.setState({
       adminConfirmPassword : e.target.value,
       showAlert : false,
@@ -85,7 +103,7 @@ class AdminControls extends Component {
     })
   }
 
-    handleAdminUsername(e){
+  handleAdminUsername(e){
     this.setState({
       adminUsername : e.target.value,
       showAlert : false,
@@ -93,134 +111,126 @@ class AdminControls extends Component {
     })
   }
 
+  handleSuccessAlert(){
+    this.setState({
+      successAlert : true
+    })
+  }
+
+  handleChangePassword(){
+
+    if (this.state.currentPassword === '' || this.state.currentPassword === ' '){
+      this.handlePassAlert('Current Password Is Required')
+    } else if (this.state.newPassword === '' || this.state.newPassword === ' '){
+      this.handlePassAlert('New Password Is Required')
+    } else if (this.state.confirmPassword === ''){
+      this.handlePassAlert('Please Confirm Password')
+    } else if (this.state.newPassword !== this.state.confirmPassword) {
+      this.handlePassAlert('Passwords Do Not Match')
+    } else {
+      axios.post('/admin/updatePassword', 
+        {id: this.props.adminId, 
+         currentPassword: this.state.currentPassword, 
+         newPassword: this.state.newPassword
+        }
+      ).then(result => {
+        if(result.data.update){
+          this.props.adminLogout();
+          browserHistory.replace('/adminLogin');
+        } else {
+          this.handlePassAlert('Current Password Is Incorrect');
+        }
+      }).catch(error => {
+        this.handlePassAlert(JSON.stringify(error));
+      })
+    }
+  }
+
+  handleNewAdmin(){
+
+    if (this.state.adminUsername === '' || this.state.adminUsername === ' ') {
+      this.handleAlert('Username Is Required')
+    } else if (this.state.adminPassword === '' || this.state.adminPassword === ' ') {
+      this.handleAlert('Password Is Required')
+    } else if (this.state.adminConfirmPassword === '') {
+      this.handleAlert('Please Confirm Password')
+    } else if (this.state.adminPassword !== this.state.adminConfirmPassword) {
+      this.handleAlert('Passwords Do Not Match')
+    } else {
+      axios.post('admin/createNewAdmin', 
+        {username: this.state.adminUsername,
+         password: this.state.adminConfirmPassword,
+         adminId : this.props.adminId
+        }
+      ).then(result => {
+        this.handleSuccessAlert();
+      }).catch(error => {
+        this.handleAlert(error.response.data.error);
+      })   
+    }
+  }
 
   render(){
 
-    const changePasswordForm = (
+    const alertInstance = (
+      <Alert bsStyle="danger">
+        <strong>{this.state.alertMessage}</strong>
+      </Alert>
+    );
 
+    const successInstance = (
+      <Alert bsStyle="success">
+        <strong>New Admin Successfully Created!</strong>
+      </Alert>
     )
+
+    const changePasswordForm = (
+        <span style={styles.option}>Change Password
+          <br/><TextField floatingLabelText="Current Password" type="password" 
+            onChange={this.handleCurrentPassword} value={this.state.currentPassword}/><br/> 
+          <TextField floatingLabelText="New Password" type="password" 
+            onChange={this.handleNewPassword} value={this.state.newPassword}/><br/>
+          <TextField floatingLabelText="Confirm New Password" type="password" 
+            onChange={this.handleConfirmPassword} value={this.state.confirmPassword}/><br/>
+        <RaisedButton onTouchTap={this.handleChangePassword} label="Submit" style={{margin : 12, float:'right'}}/><br/>
+        </span>      
+    );
 
     const newAdminForm = (
-
-    )
+        <span style={styles.option}>Create New Admin
+          <br/><TextField floatingLabelText="Username" type="text" 
+            onChange={this.handleAdminUsername} value={this.state.adminUsername}/><br/> 
+          <TextField floatingLabelText="Password" type="password" 
+            onChange={this.handleAdminPassword} value={this.state.adminPassword}/><br/>
+          <TextField floatingLabelText="Confirm Password" type="password" 
+            onChange={this.handleAdminConfirmPassword} value={this.state.adminConfirmPassword}/><br/>
+        <RaisedButton onTouchTap={this.handleNewAdmin} label="Create" style={{margin : 12, float :'right'}}/><br/>
+        </span>
+    );
 
     return (
       <div>
         <h3>Admin Controls</h3>
-        <span style={styles.option}>Change Password
-
-        </span>
         <div style={styles.divider}/>
-        <span style={styles.option}>New Admin
-
-        </span>
+        {this.state.showPassAlert ? alertInstance : ''}
+        <MuiThemeProvider>
+          {changePasswordForm}
+        </MuiThemeProvider>
+        <br/><div style={styles.divider}/>
+        {this.state.showAlert ? alertInstance : ''}
+        {this.state.successAlert ? successInstance : ''}
+        <MuiThemeProvider>
+          {newAdminForm}
+        </MuiThemeProvider>
       </div>
     )
   }
 }
 
-export default AdminControls;
-
-
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import axios from 'axios';
-import { authenticateAdmin } from '../actions/admin_actions';
-import { browserHistory } from 'react-router';
-import { TextField, RaisedButton } from 'material-ui';
-import { MuiThemeProvider } from 'material-ui/styles';
-import { Alert } from 'react-bootstrap';
-
-class AdminLogin extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      username : '',
-      usernameError : '',
-      password : '',
-      passwordError : '',
-      invalidLogin : false
-    }
-    this.handleLogin = this.handleLogin.bind(this);
-    this.handlePassword = this.handlePassword.bind(this);
-    this.handleUsername = this.handleUsername.bind(this);
-  }
-
-  handleUsername(e){
-    this.setState({
-      usernameError : '',
-      username : e.target.value,
-      invalidLogin : false
-    })
-  }
-
-  handlePassword(e){
-    this.setState({
-      passwordError : '',
-      password : e.target.value,
-      invalidLogin : false
-    })
-  }
-
-  handleLogin(){
-    if (this.state.username === '') {
-      this.setState({
-        usernameError : "Username is required"
-      })
-    } else if (this.state.password === '') {
-      this.setState({
-        passwordError : "Password is required"
-      })
-    } else {
-      let loginInfo = {username : this.state.username, password : this.state.password};
-      axios.post('/admin/adminLogin', {loginInfo : loginInfo})
-      .then(result => {
-        if (result.data.admin) {
-          this.props.authenticateAdmin(result.data.id, result.data.token);
-          browserHistory.replace('/admin');
-        } else {
-          this.setState({
-            invalidLogin : true
-          })
-        }
-      })
-      .catch(error => {
-        this.setState({
-          invalidLogin : true
-        })
-      })
-    }
-  }
-
-  render(){
-    
-    const alertInstance = (
-      <Alert bsStyle="danger">
-        <strong>Invlaid Login!</strong>
-      </Alert>
-      );
-    
-    const loginForm = (
-      <div>
-       <TextField hintText ="Username Field" floatingLabelText="Username" type="text" 
-        onChange={this.handleUsername} value={this.state.username} errorText={this.state.usernameError}/><br/> 
-       <TextField hintText="Password Field" floatingLabelText="Password" type="password" 
-        onChange={this.handlePassword} value={this.state.password} errorText={this.state.passwordError}/><br/>
-       <RaisedButton onTouchTap={this.handleLogin} label="Login" style={{margin : 12}}/><br/>
-       <div>
-       {this.state.invalidLogin ? alertInstance : ''}
-       </div>
-       </div>
-    )
-
-    return (
-      <div>
-      <MuiThemeProvider>
-        {loginForm}
-      </MuiThemeProvider>
-      </div>
-    )
+function MapStateToProps(state){
+  return {
+    adminId : state.admin.adminId
   }
 }
 
-export default connect(null, {authenticateAdmin})(AdminLogin)
+export default connect(MapStateToProps, {adminLogout})(AdminControls);
